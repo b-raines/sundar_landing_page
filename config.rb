63ce67aa8e13b4006eab8e6fabcf5a2f7e95595e
Brent Raines <bt.raines@gmail.com>
2014-02-01 11:50:18 -0500

    first commit

---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+require '/Users/user/.rvm/gems/ruby-2.1.0/gems/zen-grids-1.4/lib/zen-grids.rb'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :compressed
+environment = :production
+
+relative_assets = true
+
+color_output = false
+
+preferred_syntax = :sass
