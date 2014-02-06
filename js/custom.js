f0d30519f7118e0d361c24689aa7f1a77ab32dd9
Brent Raines <bt.raines@gmail.com>
2014-02-06 16:57:39 -0500

    add validations to signup form

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 			$('#mc-embedded-subscribe-form').attr('action', "http://sundar.us3.list-manage.com/subscribe/post?u=aa3c7ea557f2f7d7d0e073ee3&amp;id=b40988313d");
 		} else if( type.value === 'supplier') {
 			$('#mc-embedded-subscribe-form').attr('action', "http://sundar.us3.list-manage.com/subscribe/post?u=aa3c7ea557f2f7d7d0e073ee3&amp;id=ce3e25b20d");
+		} else {
+			$('#mc-embedded-subscribe-form').attr('action', '');
 		}
 
 	});
