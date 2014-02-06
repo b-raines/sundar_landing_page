ccbf710e9b424285b74924250aedbbdcbabbd50c
Brent Raines <bt.raines@gmail.com>
2014-02-06 13:15:35 -0500

    add mailchimp for designers/suppliers

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$('#type').change(function(e) {
+		e.preventDefault();
+
+		if( type.value === 'designer') {
+			$('#mc-embedded-subscribe-form').attr('action', "http://sundar.us3.list-manage.com/subscribe/post?u=aa3c7ea557f2f7d7d0e073ee3&amp;id=b40988313d");
+		} else if( type.value === 'supplier') {
+			$('#mc-embedded-subscribe-form').attr('action', "http://sundar.us3.list-manage.com/subscribe/post?u=aa3c7ea557f2f7d7d0e073ee3&amp;id=ce3e25b20d");
+		}
+
+	});
+
+});
